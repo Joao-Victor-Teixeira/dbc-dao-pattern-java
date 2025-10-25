c7f140841042d636ab762cc9e21b1b1225c9754c
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-10-25 19:43:49 -0300

    Implementa Classe Seller

--- dao_pattern_jdbc/src/application/App.java
+++ dao_pattern_jdbc/src/application/App.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.time.LocalDate;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
 
@@ -8,6 +10,9 @@ public class App {
 
         Department obj = new Department(1, "Books");
 
-        System.out.println(obj);
+        LocalDate date = LocalDate.of(1990, 1 , 12);
+        Seller seller = new Seller(21, "Bob","bob@gmail.com" , date, 3000.00, obj);
+        
+        System.out.println(seller);
 	}
 }
--- dao_pattern_jdbc/src/model/entities/Department.java
+++ dao_pattern_jdbc/src/model/entities/Department.java
@@ -38,7 +38,6 @@ public class Department implements Serializable {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
         return result;
     }
 
@@ -56,18 +55,11 @@ public class Department implements Serializable {
                 return false;
         } else if (!id.equals(other.id))
             return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
         return true;
     }
 
-    @Override
+      @Override
     public String toString() {
         return "Department [id=" + id + ", name=" + name + "]";
     }
-
-    
 }
