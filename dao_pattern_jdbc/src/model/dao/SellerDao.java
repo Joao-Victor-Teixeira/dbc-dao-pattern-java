bc778743665df86a4bc181fbc0333241d64d594a
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-10-25 20:08:00 -0300

    Implementa Interfaces SellerDao e DepartmentDao

---
+++ dao_pattern_jdbc/src/model/dao/SellerDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Seller;
+
+public interface SellerDao {
+    
+    void inset(Seller obj);
+    void update(Seller obj);
+    void deletById(Integer id);
+    Seller findById(Integer id);
+    List<Seller> findAll();
+}
