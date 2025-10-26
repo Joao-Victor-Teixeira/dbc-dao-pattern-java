23c78ba313e5a3d5f5857fc881b888347fe33318
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-10-25 21:19:42 -0300

    Implementação do findById

--- dao_pattern_jdbc/src/application/App.java
+++ dao_pattern_jdbc/src/application/App.java
@@ -1,23 +1,18 @@
 package application;
 
-import java.time.LocalDate;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
+
 import model.entities.Seller;
 
 public class App {
 
 	public static void main(String[] args) throws Exception {
 
-        Department obj = new Department(1, "Books");
-
-        LocalDate date = LocalDate.of(1990, 1 , 12);
-        Seller seller = new Seller(21, "Bob","bob@gmail.com" , date, 3000.00, obj);
-        
-        System.out.println(seller);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
 	}
 }
