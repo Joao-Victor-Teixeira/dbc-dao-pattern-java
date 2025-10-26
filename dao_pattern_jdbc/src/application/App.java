903781b457254166a3636417e2c76dc78e9dd9db
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-10-25 21:30:53 -0300

    Extrai instanciação de objetos para métodos auxiliares em SellerDaoJDBC
    Refatorado o método findById em SellerdaoJDBC para melhorar a estrutura do código e a reutilização.
    
    A lógica para criar objetos Department a partir do ResultSet foi extraída para o método privado instantiateDepartment.
    
    A lógica para criar objetos Seller (incluindo a associação do Department) foi extraída para o método privado instantiateSeller.
    
    Isso simplifica significativamente o método findById e segue o princípio DRY (Don't Repeat Yourself), preparando o código para reuso em outros métodos do DAO como findAll.

--- dao_pattern_jdbc/src/application/App.java
+++ dao_pattern_jdbc/src/application/App.java
@@ -11,8 +11,10 @@ public class App {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("*** Teste 1 : Seller findById ***");
+
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
-	}
+  }
 }
